466a6e815c2da69e0b043ce38acdb38e0719c6fd
Aqua <neku113@gmail.com>
2026-08-24 12:21:18 -0400

    Add victorialogs formula

---
+++ Formula/victorialogs.rb
@@ -0,0 +1,58 @@
+class Victorialogs < Formula
+  desc "Fast and easy to use database for logs"
+  homepage "https://docs.victoriametrics.com/victorialogs/"
+  version "1.52.0"
+  license "Apache-2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/VictoriaMetrics/VictoriaLogs/releases/download/v#{version}/victoria-logs-darwin-arm64-v#{version}.tar.gz"
+      sha256 "3157d4b6181d8a7e3e30918e2cbfcd4cc4cb66263e3ef21ea91e4f20f8980883"
+    end
+    on_intel do
+      url "https://github.com/VictoriaMetrics/VictoriaLogs/releases/download/v#{version}/victoria-logs-darwin-amd64-v#{version}.tar.gz"
+      sha256 "5ac429b81dfa007c258c537eeb63eb59bd6a8f10e8686507970c18a1b3d2dd5a"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/VictoriaMetrics/VictoriaLogs/releases/download/v#{version}/victoria-logs-linux-arm64-v#{version}.tar.gz"
+      sha256 "91338c3e5e3d743a862c0a8665bf80862f639dbd4de6f6ff19ada7df5e9acf45"
+    end
+    on_intel do
+      url "https://github.com/VictoriaMetrics/VictoriaLogs/releases/download/v#{version}/victoria-logs-linux-amd64-v#{version}.tar.gz"
+      sha256 "d14f585144b8d6813f15e11f0041f487e15e10e5f5e5a31be0311367e93d3494"
+    end
+  end
+
+  def install
+    bin.install "victoria-logs-prod" => "victoria-logs"
+  end
+
+  service do
+    run [
+      opt_bin/"victoria-logs",
+      "-httpListenAddr=127.0.0.1:9428",
+      "-storageDataPath=#{var}/victorialogs-data",
+    ]
+    keep_alive false
+    log_path var/"log/victoria-logs.log"
+    error_log_path var/"log/victoria-logs.err.log"
+  end
+
+  test do
+    http_port = free_port
+
+    pid = spawn bin/"victoria-logs",
+                "-httpListenAddr=127.0.0.1:#{http_port}",
+                "-storageDataPath=#{testpath}/victorialogs-data"
+    sleep 5
+    assert_match "VictoriaLogs", shell_output("curl -s 127.0.0.1:#{http_port}")
+
+    assert_match version.to_s, shell_output("#{bin}/victoria-logs --version")
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
