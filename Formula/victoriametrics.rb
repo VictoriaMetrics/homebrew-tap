a7ac97421fa935e6e9d24613874ca3260d93892a
Aqua <neku113@gmail.com>
2026-08-24 12:21:18 -0400

    Add victoriametrics formula

---
+++ Formula/victoriametrics.rb
@@ -0,0 +1,83 @@
+class Victoriametrics < Formula
+  desc "Cost-effective and scalable monitoring solution and time series database"
+  homepage "https://victoriametrics.com/"
+  version "1.150.0"
+  license "Apache-2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v#{version}/victoria-metrics-darwin-arm64-v#{version}.tar.gz"
+      sha256 "ed6e2c06e88fed5865fbe838d34f68613208f79032308cd12ec7d7b13785d978"
+    end
+    on_intel do
+      url "https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v#{version}/victoria-metrics-darwin-amd64-v#{version}.tar.gz"
+      sha256 "46bf0d386197990a9d21fc470021990c8f0304690b6598a474cf1fffe14c2df0"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v#{version}/victoria-metrics-linux-arm64-v#{version}.tar.gz"
+      sha256 "fdb9e272f5e4d49cb506991b7293ed53bccbff821a62653dea7c463ae1980da5"
+    end
+    on_intel do
+      url "https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v#{version}/victoria-metrics-linux-amd64-v#{version}.tar.gz"
+      sha256 "22bfe77be3de1ad03f214a005129312536d77ed4e293b66c186df417ee40a61d"
+    end
+  end
+
+  def install
+    bin.install "victoria-metrics-prod" => "victoria-metrics"
+
+    scrape_config = etc/"victoriametrics/scrape.yml"
+    return if scrape_config.exist?
+
+    scrape_config.write <<~YAML
+      global:
+        scrape_interval: 10s
+
+      scrape_configs:
+        - job_name: "victoriametrics"
+          static_configs:
+          - targets: ["127.0.0.1:8428"]
+    YAML
+  end
+
+  service do
+    run [
+      opt_bin/"victoria-metrics",
+      "-httpListenAddr=127.0.0.1:8428",
+      "-promscrape.config=#{etc}/victoriametrics/scrape.yml",
+      "-storageDataPath=#{var}/victoriametrics-data",
+    ]
+    keep_alive false
+    log_path var/"log/victoria-metrics.log"
+    error_log_path var/"log/victoria-metrics.err.log"
+  end
+
+  test do
+    http_port = free_port
+
+    (testpath/"scrape.yml").write <<~YAML
+      global:
+        scrape_interval: 10s
+
+      scrape_configs:
+        - job_name: "victoriametrics"
+          static_configs:
+          - targets: ["127.0.0.1:#{http_port}"]
+    YAML
+
+    pid = spawn bin/"victoria-metrics",
+                "-httpListenAddr=127.0.0.1:#{http_port}",
+                "-promscrape.config=#{testpath}/scrape.yml",
+                "-storageDataPath=#{testpath}/victoriametrics-data"
+    sleep 5
+    assert_match "Single-node VictoriaMetrics", shell_output("curl -s 127.0.0.1:#{http_port}")
+
+    assert_match version.to_s, shell_output("#{bin}/victoria-metrics --version")
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
